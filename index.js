ae2cfaee3b89f4d261c6cb61b1abd790a11d5ad6
Emmanuel PEREIRA <epereira777@gmail.com>
2022-09-15 17:27:29 +0200

    finalisation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cowsay = require('cowsay');
 const { data } = require('./information.js');
+const message = `Hello I'm ${data.nom} from ${data.campus}`;
 
-console.log(cowsay.say({text : data.nom, e : "O-", T : "U"}));
+console.log(cowsay.say({text : message, e : "O-", T : "U"}));
